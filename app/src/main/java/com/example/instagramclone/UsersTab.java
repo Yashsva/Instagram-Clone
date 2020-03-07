edf197658b3ccada786421bb0a06ad9f8d68c4af
Yashsva Sisodiya <namansisodiya2013@gmail.com>
2020-03-07 12:42:12 +0530

    Users Tab Fragment work Completed.

--- app/src/main/java/com/example/instagramclone/UsersTab.java
+++ app/src/main/java/com/example/instagramclone/UsersTab.java
@@ -2,12 +2,22 @@ package com.example.instagramclone;
 
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -15,6 +25,12 @@ import android.view.ViewGroup;
  */
 public class UsersTab extends Fragment {
 
+
+    private ListView listView;
+    private ArrayList  arrayList;
+    private ArrayAdapter arrayAdapter;
+
+    private TextView txtLoadingUsersData;
 
     public UsersTab() {
         // Required empty public constructor
@@ -25,7 +41,43 @@ public class UsersTab extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_users_tab, container, false);
+        View view= inflater.inflate(R.layout.fragment_users_tab, container, false);
+
+
+        listView=view.findViewById(R.id.listView);
+
+        arrayList=new ArrayList();
+
+        arrayAdapter=new ArrayAdapter(getContext(),android.R.layout.simple_list_item_1,arrayList);
+
+        txtLoadingUsersData=view.findViewById(R.id.txtLoadingUsers);
+
+        ParseQuery<ParseUser> parseQuery=ParseUser.getQuery();
+
+        parseQuery.whereNotEqualTo("usename",ParseUser.getCurrentUser().getUsername());
+
+        parseQuery.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> objects, ParseException e) {
+                if(e==null)
+                {
+                    if(objects.size()>0)
+                    {
+                        for(ParseUser user: objects)
+                        {
+                            arrayList.add(user.getUsername());
+                        }
+
+                        listView.setAdapter(arrayAdapter);
+                        txtLoadingUsersData.animate().alpha(0).setDuration(2000);
+                        listView.setVisibility(View.VISIBLE);
+                    }
+                }
+            }
+        });
+
+
+        return view;
     }
 
 }
